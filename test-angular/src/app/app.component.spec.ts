acfac0cbfa4d5c2d7802653685991b39f4aad59a
Pedro S Santos <pedrosalviano170@gmail.com>
2025-04-10 20:52:09 -0300

    hotfix typo on nome

--- test-angular/src/app/app.component.spec.ts
+++ test-angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'calculadora-angular' title`, () => {
+  it(`should have the 'test-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculadora-angular');
+    expect(app.title).toEqual('test-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculadora-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test-angular');
   });
 });
